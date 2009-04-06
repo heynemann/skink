5d1a5e9d0d31a4d8a93f84eb171e73f688fb4cc6
Bernardo Heynemann <bernardo@corp.globo.com>
2009-04-06 15:51:55 -0300

    Layout stuff - eek

--- skink/media/script/create_project.js
+++ skink/media/script/create_project.js
@@ -3,4 +3,11 @@ $(function(){
         window.location='/';
         return false;
     });
+
+    $('.rounded').corners({ 
+        inColor: '#e0ffda', 
+        outColor: '#e0ffda',
+        borderSize: 2, 
+        borderColor: '#307822'
+    });
 });
