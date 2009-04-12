2193997f3a9015c41e8bbb8f17be08f3b7cfeefe
Claudio Figueiredo <jcfigueiredo@gmail.com>
2009-04-12 19:20:18 -0300

    setting base testes

--- skink/plugins/email_plugin.py
+++ skink/plugins/email_plugin.py
@@ -6,6 +6,8 @@ class EmailPlugin (BasePlugin):
 
     def __init__(self,configuration):
         Guard.against_empty(configuration.get('smtp_host',None))
+        Guard.against_empty(configuration.get('smtp_user',None))
+        Guard.against_empty(configuration.get('smtp_passwd',None))
         
         self.configuration = Dict(configuration)
 
--- tests/unit/test_email_plugin.py
+++ tests/unit/test_email_plugin.py
@@ -10,9 +10,17 @@ from tests.base.base_unit_test import BaseUnitTest
 from skink.plugins import BasePlugin
 from skink.plugins.email_plugin import EmailPlugin
 from hamcrest import *
+#from nope import dope
 
 def TestEmailPlugin(BaseUnitTest):
-    pass
-    
+    def do_nothing(self):
+        self.done = True
+        
+    def test_null_configuration(self):
+        assert_that(EmailPlugin(),raises(ValueError))
+        self.asserRaises(ValueError,EmailPlugin())
+        
+        fail('Should have raised a ValueError')
+
 if __name__ == '__main__':
     unittest.main()
