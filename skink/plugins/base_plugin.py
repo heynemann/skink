dc83a69f1d1aad2b25064975961d74248ee74969
Claudio Figueiredo <jcfigueiredo@gmail.com>
2009-04-12 14:00:10 -0300

    adding base methods

--- skink/plugins/base_plugin.py
+++ skink/plugins/base_plugin.py
@@ -1,28 +1,2 @@
 class BasePlugin (object):
-    def OnProjectCreated(self, project):
-        pass
-
-    def OnProjectUpdated(self, project):
-        pass
-
-    def OnProjectDeleted(self, project):
-        pass
-
-    def OnPipelineCreated(self, pipeline):
-        pass
-
-    def OnPipelineUpdated(self, pipeline):
-        pass
-
-    def OnPipelineDeleted(self, pipeline):
-        pass
-
-    def OnBeforeBuild(self, project):
-        pass
-
-    def OnBuildSuccessful(self, project, build):
-        pass
-
-    def OnBuildFailed(self, project, build):
-        pass
-
+    pass
