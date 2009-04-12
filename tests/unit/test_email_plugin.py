6c6db3ad5484678a1a966e9f3d3a6bbf91761d79
Claudio Figueiredo <jcfigueiredo@gmail.com>
2009-04-12 19:50:40 -0300

    referencing Plugin instead of BasePlugin

--- tests/unit/test_email_plugin.py
+++ tests/unit/test_email_plugin.py
@@ -7,7 +7,7 @@ root_path = abspath(join(dirname(__file__), "../../"))
 sys.path.insert(0, root_path)
 
 from tests.base.base_unit_test import BaseUnitTest
-from skink.plugins import BasePlugin
+from skink.plugins import Plugin
 from skink.plugins.email_plugin import EmailPlugin
 from hamcrest import *
 #from nope import dope
