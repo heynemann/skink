96b33d74935e39f30a4567ed79c809ce83b35244
Bernardo Heynemann <heynemann@gmail.com>
2009-04-13 02:12:33 -0300

    Fixed e-mail plugin.

--- skink/plugins/email_plugin.py
+++ skink/plugins/email_plugin.py
@@ -28,24 +28,35 @@ class EmailPlugin (Plugin):
         self.smtp_user = self.configuration.get('smtp_user',None)
         self.smtp_pass = self.configuration.get('smtp_pass',None)
         self.email_sender = self.configuration.get('email_sender',None)
-        self.email_recipients = self.configuration.get('email_recipients',None)
+        recipients = self.configuration.get('email_recipients', None) 
+        self.email_recipients = recipients and recipients.split(';') or None
 
         Guard.against_empty(self.smtp_host, no_host_message)
         Guard.against_empty(self.smtp_user, no_user_message)
         Guard.against_empty(self.smtp_pass, no_pass_message)
         Guard.against_empty(self.email_sender, no_sender_message)
         Guard.against_empty(self.email_recipients, no_recipients_message)
-        
-        self.__smpt_session = self.__create_smtp_session__(self.smtp_host,self.smtp_user,self.smtp_pass)
-        
+
     def __create_smtp_session__(self,host,user,password):
           session = smtplib.SMTP(host)
           session.login(user, password)
           return session
 
-    def __send_email__(self, msg):
+    def __send_email__(self, subject, msg):
         try:
-            smtp_result = self.__smpt_session.sendmail(self.email_sender, self.email_recipients, msg)
+            smtp_session = self.__create_smtp_session__(self.smtp_host,self.smtp_user,self.smtp_pass)
+            smtp_session.set_debuglevel(1)
+            recipients = ", ".join([recipient.strip() for recipient in self.email_recipients])
+            body = "\r\n".join((
+                                "From: %s" % self.email_sender,
+                                "To: %s" % recipients,
+                                "Subject: %s" % subject,
+                                "",
+                                msg
+                              ))
+
+            smtp_result = smtp_session.sendmail(self.email_sender, recipients, body)
+
             if smtp_result:
                 errstr = ""
                 for recip in smtp_result.keys():
@@ -83,8 +94,10 @@ class EmailPlugin (Plugin):
 
     def on_build_successful(self, project, build):
         print(" ==> on_build_successful")
-        self.__send_email__("project [%s], build [%s] successfully :) " %(project.name,build.number))
+        self.__send_email__("[Skink] PROJECT %s was built successfully" % project.name, 
+                            "project [%s], build [%s] successfully :) " %(project.name,build.number))
 
     def on_build_failed(self, project, build):
         print(" ==> on_build_failed")
-        self.__send_email__("project [%s], build [%s] failed :(" %(project.name,build.number))
+        self.__send_email__("[Skink] PROJECT %s failed the build" % project.name, 
+                            "project [%s], build [%s] failed :(" %(project.name,build.number))
