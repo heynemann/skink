a55f5e473be041291c2c8201d18fe206d2ba6bfe
Bernardo Heynemann <heynemann@gmail.com>
2009-04-12 22:38:26 -0300

    Added skink_console.py

---
+++ skink/skink_console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+u"""Skink - Simple Fun Continuous Integration Server
+ 
+Example usage
+=============
+ 
+    python skink_console.py (or skink_console.py after you make it executable)
+ 
+authors: `Bernardo Heynemann <heynemann@gmail.com> and José Cláudio Figueiredo <jcfigueiredo@gmail.com>`
+"""
+__revision__ = "$Id$"
+__docformat__ = 'restructuredtext en'
+ 
+import os
+import optparse
+import sys
+from os.path import dirname, abspath, join
+root_path = abspath(join(dirname(__file__), "../"))
+sys.path.insert(0, root_path)
+
+from skink.controllers.infra import Server
+
+def main():
+    """ Main function - parses args and runs action """
+    parser = optparse.OptionParser(usage="%prog or type %prog -h (--help) for help", description=__doc__, version="%prog" + __revision__)
+    #parser.add_option("-p", "--pattern", dest="pattern", default="*.acc", help="File pattern. Defines which files will get executed [default: %default].")
+
+    (options, args) = parser.parse_args()
+ 
+    Server.start()
+    
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
