bbc8592198680b2095ffe5a57a5b367086f1d084
Bernardo Heynemann <bernardo@corp.globo.com>
2009-06-09 13:48:20 -0300

    Fixed running in windows.

--- skink/services/executers.py
+++ skink/services/executers.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import commands
+import os
+from subprocess import Popen, PIPE
 
 class ShellExecuter(object):
     def execute(self, command, base_path, change_dir=True):
+        print ("Executing command: %s" % command)
         complement=""
         if change_dir:
-            complement = "cd %s && " % base_path
-        
-        result = commands.getstatusoutput("%s%s" % (complement, command))
-        return ExecuteResult(command, result[1], result[0])
+            oldpath = os.path.abspath(os.curdir)
+            if os.path.exists(base_path):
+                os.chdir(base_path)
+
+        arguments = os.name == "nt" and command.split(" ") or [command]
+
+        proc = Popen(command.split(" "), stdout=PIPE, stderr=PIPE)
+        log = "\n".join(proc.communicate())
+        exit_code = proc.returncode
+        if change_dir:
+            os.chdir(oldpath)
+        return ExecuteResult(command, log, exit_code)
 
 class ExecuteResult(object):
     def __init__(self, command, run_log, exit_code):
