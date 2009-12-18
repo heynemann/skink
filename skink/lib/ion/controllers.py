90417a6bf7f3650322458f2662f8d2e6b3b46645
Bernardo Heynemann <bernardo@corp.globo.com>
2009-12-18 12:01:08 -0200

    Controllers tested

--- skink/lib/ion/controllers.py
+++ skink/lib/ion/controllers.py
@@ -15,5 +15,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__CONTROLLERS__ = []
+__CONTROLLERSDICT__ = {}
+
+def route(route, name=None):
+    def dec(func):
+        actual_name = func.__name__
+        if name:
+            actual_name = name
+        conf = (
+            actual_name, {
+                'route': route,
+                'method': func.__name__
+            }
+        )
+        return func, conf
+
+    return dec
+
+class MetaController(type):
+    def __init__(cls, name, bases, attrs):
+        if name not in ('MetaController', 'Controller'):
+            __CONTROLLERS__.append(cls)
+            __CONTROLLERSDICT__[name] = cls
+            cls.__routes__ = []
+            for attr, value in attrs.items():
+                if isinstance(value, tuple) and len(value) is 2:
+                    method, conf = value
+                    setattr(cls, attr, method)
+                    cls.__routes__.append(conf)
+
+        super(MetaController, cls).__init__(name, bases, attrs)
+
 class Controller(object):
-    pass
+    __metaclass__ = MetaController
+    __routes__ = None
+    context = None
+
+    @classmethod
+    def all(self):
+        return __CONTROLLERS__
+
+#    def register_routes(self, dispatcher):
+#        for route in self.__routes__:
+#            route_name = "%s_%s" % (get_controller_name(self.__class__.__name__), route[0])
+#            dispatcher.connect(route_name, route[1]["route"], controller=self, action=route[1]["method"])
+
--- tests/unit/ion/test_controllers.py
+++ tests/unit/ion/test_controllers.py
@@ -15,7 +15,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from skink.lib.ion.controllers import Controller
+import skink.lib.ion.controllers as ctrl
+from skink.lib.ion.controllers import Controller, route
+
+def clear():
+    ctrl.__CONTROLLERS__ = []
+    ctrl.__CONTROLLERSDICT__ = {}
 
 def test_can_create_controller():
     ctrl = Controller()
+    assert ctrl
+
+def test_controller_has_null_context_by_default():
+    ctrl = Controller()
+
+    assert not ctrl.context
+
+def test_controller_has_empty_routes_by_default():
+    clear()
+    class TestController(Controller):
+        pass
+
+    ctrl = TestController()
+
+    assert ctrl.__routes__ is not None
+    assert not ctrl.__routes__
+    assert isinstance(ctrl.__routes__, list)
+
+def test_all_controllers_returns_all_imported_controllers():
+    clear()
+    class TestController2(Controller):
+        pass
+
+    controllers = Controller.all()
+
+    assert controllers
+    assert len(controllers) == 1
+    assert controllers[0] == TestController2
+
+def test_route_decorator_registers_route_information():
+    clear()
+    class TestController(Controller):
+        @route("/something")
+        def SomeAction(self):
+            pass
+
+    assert TestController.__routes__
+
+    #Example of a route
+    #('SomeAction', {'route': '/something', 'method': 'SomeAction'})
+    assert TestController.__routes__[0][0] == 'SomeAction'
+    assert TestController.__routes__[0][1]['route'] == '/something'
+    assert TestController.__routes__[0][1]['method'] == 'SomeAction'
+
+def test_route_decorator_registers_route_with_custom_name():
+    clear()
+    class TestController(Controller):
+        @route("/something", name="named_route")
+        def SomeAction(self):
+            pass
+
+    assert TestController.__routes__
+
+    assert TestController.__routes__[0][0] == 'named_route'
+    assert TestController.__routes__[0][1]['route'] == '/something'
+    assert TestController.__routes__[0][1]['method'] == 'SomeAction'
+
